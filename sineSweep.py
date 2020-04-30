28873a7ea6eacbbd0d9560f28457ac2d880f0ccb
Franco Rebora <francor.96@gmail.com>
2020-04-30 17:36:25 -0300

    Hicimos entre los 3 el invFilter

--- sineSweep.py
+++ sineSweep.py
@@ -1,22 +1,40 @@
 import numpy as np
-from scipy import signal
 import soundfile as sf
 import sounddevice as sd
 
-def sineSweep(t, f1, f2):
+def sineSweep(t, f1, f2, inv = False):
+    '''Esto Esto un docstring del SineSweep.'''
     w1 = 2*np.pi*f1
     w2 = 2*np.pi*f2
     
     fs = 44100
     
     nx = t*fs
-    x = np.linspace(0,t,nx)
+    vectorT = np.linspace(0,t,nx)
     k = (t*w1)/np.log(w2/w1)
     l = t/np.log(w2/w1)
+  
+    if inv == False:
+        sineSweep = 0.5*np.sin(k*(np.exp(vectorT/l)-1))
+        sf.write("sineSweep.wav", sineSweep, fs)
+        return sineSweep
     
-    sineSweep = np.sin(k*(np.exp(x/l)-1))
+    elif inv == True: 
+        w = k/l * np.exp(vectorT/l)
+        m = w1/(2*np.pi*w)
+        vectorT = -vectorT
+        sineSweep = 0.5*np.sin(k*(np.exp(vectorT/l)-1))
+        invFilter = m * sineSweep  
+        sf.write("invFilter.wav", invFilter, fs)
+        return invFilter 
     
-    sf.write("sineSweep.wav", sineSweep, fs)
-    sd.play(sineSweep)
+    else:
+        return 'Invalid Argument.'
     
-sineSweep(5,500,3000)
+    
+    
+    
+
+
+
+    
