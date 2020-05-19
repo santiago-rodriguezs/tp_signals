c67bf81e4adad60267ffde2d1bb7381f78932454
Franco Rebora <francor.96@gmail.com>
2020-05-19 18:37:47 -0300

    Signed-off-by: Franco Rebora <francor.96@gmail.com>

--- importDataset.py
+++ importDataset.py
@@ -1,15 +1,37 @@
 import soundfile as sf
 import numpy as np
 
-def read(*args):
+def readwavs(*audios):
     '''
-    Docstring goes here.
+    Import a variable number of audios in format `.wav` and returns a 
+    dictionary containing each audio with data type `ndarray` from numpy.
+    
+    
+    Parameters
+    ----------
+    *audio : str, variable number of inputs.
+        Name of audio file `.wav` in PATH.
+    
+    
+    Example
+    -------
+    Import three audios.
+    
+        import soundfile as sf
+        import numpy as np
+                
+        database = readwavs()     
     '''
     database = {}
     
-    for i in args:
+    for i in audios:
         database[i] = sf.read(i)
     
     return database
 
-waves = read('test1.wav','test2.wav')
+
+# #----- TEST: reading two files in one line.
+    
+# waves = readwavs('test1.wav','test2.wav')
+
+# # END OF TEST
