4d21502f938120a7e4d349d2e2e80c46890f5b1e
Franco Rebora <francor.96@gmail.com>
2020-05-29 13:17:49 -0300

    fix logNorm

--- logNorm.py
+++ logNorm.py
@@ -1,18 +1,39 @@
 import numpy as np
 
 def logNorm(audio):
-    '''This function takes an audio input and returns it converted to a logarithmic scale'''
+    '''
+    Takes an audio input and returns it converted to a dbFS scale.
+    This function is meant to be used with numpy arrays.
+    
+    Parameters
+    ----------
+    audio : ndarray
+        Numpy array containing the signal.
+
+    Returns
+    -------
+    logNorm : ndarray
+        Returns the same signal coverted to a dbFS scale.
+        
+    Example
+    -------
+    import numpy as np
+    import matplotlib.pyplot as plt
+    audio = np.random.randn(48000)
+    logNorm = logNorm(audio)
+    plt.plot(logNorm)
+
+    '''
     if 0 in audio:
         minimum = min(i for i in audio if i > 0)
 
         audio[audio == 0] = minimum
             
 
-    logNorm = 20 * np.log10(audio/max(audio)) 
+    logNorm = 20 * np.log10((audio/max(audio))**2) 
    
     return logNorm
 
-    
 
 
 
